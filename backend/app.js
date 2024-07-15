25cec920f26cbb9486964d5a7a916e26141595ad
ambika <ambikashivam2003@gmail.com>
2024-07-15 23:50:28 +0530

    rourter done and error handling

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import { errorMiddleware } from "./middlewares/error.js";
-// import reservationRouter from "./routes/reservationRoute.js";
+import { errorMiddleware } from "./error/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./db/dbConnection.js";
 
 const app = express();
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/v1/reservation", reservationRouter);
+app.use("/api", reservationRouter);
 // app.get("/", (req, res, next)=>{return res.status(200).json({
 //   success: true,
 //   message: "HELLO WORLD AGAIN"
